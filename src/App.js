3d3905b7c788c0b2f8994ecc904e5089bb0abec3
RamonaS2 <bandeiraramona2@gmail.com>
2022-05-11 19:38:46 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+import ProfileEdit from './ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTune</p>);
+    return (
+      <BrowserRouter>
+        <Login />
+        <Search />
+        <Album />
+        <Favorites />
+        <Profile />
+        <ProfileEdit />
+        <NotFound />
+      </BrowserRouter>
+    );
   }
 }
 
