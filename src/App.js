b531f865f769d0261e4bdd369d8d41db16cc6763
RamonaS2 <bandeiraramona2@gmail.com>
2022-05-12 09:11:38 -0300

    tentando descobrir o erro do valor da input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -12,13 +12,17 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Login />
-        <Search />
-        <Album />
-        <Favorites />
-        <Profile />
-        <ProfileEdit />
-        <NotFound />
+        <div>
+          <Switch>
+            <Route exact path="/"><Login /></Route>
+            <Route path="/Search"><Search /></Route>
+            <Route path="/Album"><Album /></Route>
+            <Route path="/Favorites"><Favorites /></Route>
+            <Route exact path="/Profile"><Profile /></Route>
+            <Route path="/Profile/edit"><ProfileEdit /></Route>
+            <Route><NotFound /></Route>
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,50 @@
 import React from 'react';
+// import { createUser } from './services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+    };
+
+    this.valueInput = this.valueInput.bind(this);
+  }
+
+  valueInput({ target }) {
+    const { value } = target;
+
+    this.setState = ({
+      user: value,
+    });
+  }
+
   render() {
+    const { user } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <h3>Login</h3>
+        <form>
+          <label htmlFor="nome">
+            Usuário:
+            <input
+              id="nome"
+              type="text"
+              name="user"
+              value={ user }
+              data-testid="login-name-input"
+              onChange={ this.valueInput }
+            />
+          </label>
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            // onClick={ this. }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
