4d9520e1bb55ce2581f3d3efe0bf1c1e684ef213
RamonaS2 <bandeiraramona2@gmail.com>
2022-05-13 00:42:23 -0300

    requisito 14

--- src/ProfileEdit.js
+++ src/ProfileEdit.js
@@ -1,11 +1,145 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from './components/Header';
+import { getUser, updateUser } from './services/userAPI';
+import Carregando from './pages/Carregando';
 
 class ProfileEdit extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: {},
+      loanding: false,
+      disabled: true,
+      redirect: false,
+    };
+  }
+
+  componentDidMount() {
+    this.usuario();
+  }
+
+  onInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState((prevState) => ({
+      user: {
+        ...prevState.user,
+        [name]: value,
+      },
+    }));
+    this.validandoButton();
+  }
+
+  onButton = () => {
+    const { user } = this.state;
+    this.setState({
+      loanding: true,
+    }, async () => {
+      await updateUser(user);
+      this.setState({ redirect: true });
+    });
+  }
+
+  usuario = () => {
+    this.setState({ loanding: true }, async () => {
+      this.setState({ user: await getUser(), loanding: false }, () => {
+        this.validandoButton();
+      });
+    });
+  }
+
+  validandoButton = () => {
+    const { user } = this.state;
+    const { name, email, description, image } = user;
+    const validateEmail = (props) => {
+      const verify = /\S+@\S+\.\S+/;
+      return verify.test(props);
+    };
+    if (
+      name !== ''
+      && email !== ''
+      && validateEmail(email)
+      && description !== ''
+      && image !== ''
+    ) this.setState({ disabled: false });
+    else this.setState({ disabled: true });
+  }
+
   render() {
+    const { loanding, user, disabled, redirect } = this.state;
+    const { name, email, description, image } = user;
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        {loanding ? <Carregando />
+          : (
+            <div>
+              <div>
+                <label htmlFor="name">
+                  Nome:
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    defaultValue={ name }
+                    onChange={ this.onInput }
+                    data-testid="edit-input-name"
+                  />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="email">
+                  Email:
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    defaultValue={ email }
+                    onChange={ this.onInput }
+                    data-testid="edit-input-email"
+                  />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="description">
+                  Descrição:
+                  <input
+                    id="description"
+                    name="description"
+                    type="text"
+                    defaultValue={ description }
+                    onChange={ this.onInput }
+                    data-testid="edit-input-description"
+                  />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="image">
+                  Mudar Foto:
+                  <input
+                    id="image"
+                    name="image"
+                    type="text"
+                    defaultValue={ image }
+                    onChange={ this.onInput }
+                    data-testid="edit-input-image"
+                  />
+                </label>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  disabled={ disabled }
+                  data-testid="edit-button-save"
+                  onClick={ this.onButton }
+                >
+                  Salvar
+                </button>
+              </div>
+            </div>
+          )}
+        {redirect === true && <Redirect exact to="/profile" />}
       </div>
     );
   }
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Carregando from './Carregando';
-// import MusicCard from './MusicCard';
+import MusicCard from './MusicCard';
 
 class Favorites extends React.Component {
   constructor() {
     super();
     this.state = {
       loanding: false,
-      favoritas: [],
+      fav: [],
     };
   }
 
@@ -17,47 +17,37 @@ class Favorites extends React.Component {
     this.favorite();
   }
 
-  favorite = () => {
+  favorite = async () => {
     this.setState({
       loanding: true,
-    }, async () => {
-      const list = await getFavoriteSongs();
-      this.setState({
-        loanding: false,
-        favoritas: list,
-      });
+    });
+    const result = await getFavoriteSongs();
+    this.setState({
+      fav: result,
+      loanding: false,
     });
   }
 
   render() {
-    const { loanding, favoritas } = this.state;
+    const { loanding, fav } = this.state;
     return loanding ? <Carregando />
       : (
         <div data-testid="page-favorites">
           <Header />
-          <p>{`${favoritas}`}</p>
-          {/* {
-            favoritas.map((musica, i) => (
+          {
+            fav.map((musica, i) => (
               <MusicCard
                 key={ i }
                 trackName={ musica.trackName }
-                previewUrl={ musica.previewUrl }
                 trackId={ musica.trackId }
-                onChange={ () => ({
-                  this.setState({
-                    loanding: true,
-                  }, async () => ({
-                    const list = await getFavoriteSongs();
-                    this.setState({
-                      loanding: false,
-                      favoritas: list,
-                    })
-                  }))
-                }) }
-                checked={ favorites.some((e) => e.trackId === musica.trackId) }
+                previewUrl={ musica.previewUrl }
+                song={ musica }
+                update={ this.favorite }
+                checked={ fav.some((e) => e.trackId === musica.trackId) }
+
               />
             ))
-          } */}
+          }
         </div>
       );
   }
