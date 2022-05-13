e824e7a822e5f6bfcef4722ad931091caba3d77c
RamonaS2 <bandeiraramona2@gmail.com>
2022-05-12 21:34:08 -0300

    requisito 13

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,13 +1,65 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
+// import MusicCard from './MusicCard';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loanding: false,
+      favoritas: [],
+    };
+  }
+
+  componentDidMount() {
+    this.favorite();
+  }
+
+  favorite = () => {
+    this.setState({
+      loanding: true,
+    }, async () => {
+      const list = await getFavoriteSongs();
+      this.setState({
+        loanding: false,
+        favoritas: list,
+      });
+    });
+  }
+
   render() {
-    return (
-      <div data-testid="page-favorites">
-        <Header />
-      </div>
-    );
+    const { loanding, favoritas } = this.state;
+    return loanding ? <Carregando />
+      : (
+        <div data-testid="page-favorites">
+          <Header />
+          <p>{`${favoritas}`}</p>
+          {/* {
+            favoritas.map((musica, i) => (
+              <MusicCard
+                key={ i }
+                trackName={ musica.trackName }
+                previewUrl={ musica.previewUrl }
+                trackId={ musica.trackId }
+                onChange={ () => ({
+                  this.setState({
+                    loanding: true,
+                  }, async () => ({
+                    const list = await getFavoriteSongs();
+                    this.setState({
+                      loanding: false,
+                      favoritas: list,
+                    })
+                  }))
+                }) }
+                checked={ favorites.some((e) => e.trackId === musica.trackId) }
+              />
+            ))
+          } */}
+        </div>
+      );
   }
 }
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Carregando from './Carregando';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loanding: false,
+      description: '',
+      name: '',
+      email: '',
+      image: '',
+    };
+  }
+
+  componentDidMount() {
+    this.usuario();
+  }
+
+  usuario = () => {
+    this.setState({
+      loanding: true,
+    }, async () => {
+      const user = await getUser();
+      // console.log(user);
+      const { name, description, email, image } = user;
+      this.setState({
+        name,
+        description,
+        email,
+        image,
+        loanding: false,
+      });
+    });
+  }
+
   render() {
+    const { name, description, email, image, loanding } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { loanding ? <Carregando /> : (
+          <div>
+            <h3>Foto:</h3>
+            <img
+              src={ image }
+              alt={ name }
+              data-testid="profile-image"
+            />
+            <h3>Nome:</h3>
+            <p>{name}</p>
+            <h3>Email:</h3>
+            <p>{email}</p>
+            <h3>Descrição</h3>
+            <p>{description}</p>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        )}
       </div>
     );
   }
